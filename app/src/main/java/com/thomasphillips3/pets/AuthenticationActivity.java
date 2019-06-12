55ffaca8661ee0c5ac41afdd515b3a871eb29c9d
Thomas Phillips <thomasphillips3@gmail.com>
2019-06-11 23:21:54 -0700

    adds auth ui

---
+++ app/src/main/java/com/thomasphillips3/pets/AuthenticationActivity.java
@@ -0,0 +1,56 @@
+package com.thomasphillips3.pets;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.amazonaws.mobile.client.AWSMobileClient;
+import com.amazonaws.mobile.client.Callback;
+import com.amazonaws.mobile.client.SignInUIOptions;
+import com.amazonaws.mobile.client.UserStateDetails;
+
+public class AuthenticationActivity extends AppCompatActivity {
+
+    private final String TAG = AuthenticationActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authentication);
+
+        AWSMobileClient.getInstance().initialize(getApplicationContext(), new Callback<UserStateDetails>() {
+            @Override
+            public void onResult(UserStateDetails userStateDetails) {
+                Log.i(TAG, userStateDetails.getUserState().toString());
+                switch (userStateDetails.getUserState()) {
+                    case SIGNED_IN:
+                        Intent i = new Intent(AuthenticationActivity.this, MainActivity.class);
+                        startActivity(i);
+                        break;
+                    case SIGNED_OUT:
+                        showSignIn();
+                        break;
+
+                        default:
+                            AWSMobileClient.getInstance().signOut();
+                            break;
+                }
+            }
+
+            @Override
+            public void onError(Exception e) {
+                Log.e(TAG, e.toString());
+            }
+        });
+    }
+
+    private void showSignIn() {
+        try {
+            AWSMobileClient.getInstance().showSignIn(this, SignInUIOptions.builder().nextActivity(MainActivity.class).build());
+        } catch (Exception e) {
+            Log.e(TAG, e.toString());
+        }
+    }
+}
