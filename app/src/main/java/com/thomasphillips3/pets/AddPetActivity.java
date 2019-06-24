fa5c3d41443e18a9b619f5062066203d68a7793e
Thomas Phillips <thomasphillips3@gmail.com>
2019-06-23 22:32:11 -0700

    implements choosing a photo

--- app/src/main/java/com/thomasphillips3/pets/AddPetActivity.java
+++ app/src/main/java/com/thomasphillips3/pets/AddPetActivity.java
@@ -1,9 +1,13 @@
 package com.thomasphillips3.pets;
 
 import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -30,6 +34,29 @@ import type.CreatePetInput;
 
 public class AddPetActivity extends AppCompatActivity {
     private static final String TAG = AddPetActivity.class.getSimpleName();
+    private static int RESULT_LOAD_IMAGE = 1;
+    private String photoPath;
+
+    public void choosePhoto() {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, RESULT_LOAD_IMAGE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode ==  RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = {MediaStore.Images.Media.DATA};
+            Cursor cursor = getContentResolver().query(selectedImage,
+                    filePathColumn, null, null, null);
+            cursor.moveToFirst();
+            int columnIndex =  cursor.getColumnIndex(filePathColumn[0]);
+            String picturePath = cursor.getString(columnIndex);
+            cursor.close();
+            photoPath = picturePath;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +68,14 @@ public class AddPetActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 save();
+            }
+        });
+
+        Button buttonAddPhoto = findViewById(R.id.button_add_photo);
+        buttonAddPhoto.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                choosePhoto();
             }
         });
     }
